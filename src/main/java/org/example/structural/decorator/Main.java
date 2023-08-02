b65f6ac0ceede0500b6cf196f6f991b7ec3beb49
DESKTOP-4T410RF\Dell <porchelvigunasekaran@gmail.com>
2023-08-02 11:06:01 +0200

    Added Rs with cost

--- src/main/java/org/example/structural/decorator/Main.java
+++ src/main/java/org/example/structural/decorator/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         BasePizza pizza = new ExtraCheese(new VegDelight());
-        System.out.println("Pizza Price "+pizza.cost());
+        System.out.println("Pizza Price "+pizza.cost()+"Rs");
     }
 }
